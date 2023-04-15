bbeae3cb1949bf520339c096a5e37563ca73390d
Gregory <maklakov.grigory@gmail.com>
2023-04-15 13:56:37 +0200

    add font & vars | add tailwind plugins⚡️

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -11,8 +6,6 @@ export default function Home() {
       <Head>
         <title>Ginger Beauty Zone</title>
         <meta name="description" content="Teren Twojego Piękna" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="">home</main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-inter)", ...fontFamily.sans],
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
       },
     },
   },
