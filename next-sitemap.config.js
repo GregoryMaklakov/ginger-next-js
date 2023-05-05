2f11ea20e6b50027a7df1451f402ea3583af1023
Gregory <maklakov.grigory@gmail.com>
2023-05-05 12:21:35 +0200

    add sitemap & postbuild

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://ginger-beauty-zone.com/",
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,
+};
