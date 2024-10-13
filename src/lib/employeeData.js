4a99880fc87287bc665406a9fe5e10589aeb1baa
Gregory <maklakov.grigory@gmail.com>
2024-10-13 14:55:59 +0200

    add eyebrows offer

--- src/lib/employeeData.js
+++ src/lib/employeeData.js
@@ -32,6 +32,7 @@ export const employeeData = [
 ];
 
 export const hiringEmployeesData = [
+  // Mani+Pedi
   {
     slug: "mani-offer",
     title: "Manikiurzystka - Stylistka paznokci",
@@ -102,12 +103,75 @@ export const hiringEmployeesData = [
       ],
     },
   },
+  // Brwi + Rzęsy
   {
     slug: "eyebrow-lash-offer",
     title: "Stylista Brwi, Rzęs / Wizażysta",
     gradientWord: "Stylista",
-    subtitle: "Pełny etat",
-    disabled: true,
+    subtitle: "Pełny etat/Сzęść etatu",
+    disabled: false,
+    imgURL: "/images/work/offers/offer-01.jpg",
+    resultThree: [
+      {
+        name: "Dlaczego Ginger Beauty Zone?",
+        data: [
+          {
+            before: " Już ponad",
+            amount: "2",
+            subtitle: "lata na rynku beauty",
+          },
+          {
+            before: "ponad",
+            amount: "1480",
+            subtitle: "zadowolonych klientów",
+          },
+          {
+            before: "ponad",
+            amount: "41000",
+            subtitle: "wykonanych usług",
+          },
+        ],
+      },
+    ],
+
+    blockAccordion: [
+      {
+        heading: "Szczegóły",
+        data: [
+          {
+            num: "/",
+            subtitle: "O nas",
+            text: "Ginger Beauty Zone to dynamiczny i profesjonalny salon piękności w centrum Warszawy, znany z wysokiej jakości usług i przyjaznej atmosfery. Nasza misja to nie tylko dbanie o piękno naszych klientów, ale także zapewnienie najlepszych warunków pracy dla naszego zespołu.",
+          },
+          {
+            num: "/",
+            subtitle: "Kogo Szukamy",
+            text: "Poszukujemy doświadczonej Stylistki Brwi i Rzęs, która dołączy do naszego zespołu. Jeśli masz pasję do swojej pracy, chcesz się rozwijać i lubisz pracować w przyjaznym środowisku, ta oferta jest dla Ciebie!",
+          },
+        ],
+      },
+    ],
+    servicesAdvantage: {
+      heading: "Twój zakres obowiązków:",
+      list: [
+        "Profesjonalne modelowanie brwi, henna oraz laminacja brwi.",
+        "Wykonywanie zabiegów przedłużania i laminacji rzęs.",
+        "Dbanie o estetykę i komfort klienta podczas zabiegów.",
+        "Minimum 6 miesięcy doświadczenia na podobnym stanowisku.",
+        "Umiejętności komunikacyjne i dbałość o szczegóły.",
+        "Chęć ciągłego rozwoju i nauki nowych technik.",
+      ],
+    },
+    servicesAdvantageSecond: {
+      heading: "Co Oferujemy:",
+      list: [
+        "Atrakcyjne wynagrodzenie premie za wyniki.",
+        "Pełny etat lub część etatu, oficjalne zatrudnienie.",
+        "Materiały studia i profesjonalny rozwój.",
+        "Świetną atmosferę pracy i regularne imprezy korporacyjne.",
+        "Wygodne i nowoczesne miejsce pracy.",
+      ],
+    },
   },
   {
     slug: "administrator-offer",
